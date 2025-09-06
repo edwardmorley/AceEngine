ffb99953cd5ec876e8866398aa83bad495ab9287
edwardmorley <edwardmorley2010@icloud.com>
2025-09-07 00:27:38 +0100

    Update gamelogic.py
    added variables for emotions and more game logic and added detection for capturing

--- gamelogic.py
+++ gamelogic.py
@@ -1,14 +1,28 @@
-turn = True
-# True = White's turn
+whiteturn = True
+whitecastling = True
+blackcastling = True
 
 depth = 1
 
+agressive = 0.0
+modest = 0.0
+tactful = 0.0
+positional = 0.0
+safe = 0.0
+aware = 0.0
+controlling = 0.0
+
 PAWNVALUE = 1.0
-KNIGHTVALUE = 3.0
-BISHOPVALUE = 3.15
-ROOKVALUE = 5.0
-QUEENVALUE = 9.0
-KINGVALUE = 100.0
+KNIGHTVALUE = 6.2
+BISHOPVALUE = 6.55
+ROOKVALUE = 10.13
+QUEENVALUE = 20.14
+KINGVALUE = 200.0
+
+PAWNSPACE = 8
+PIECESPACE = [0.7, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.7, 0.8, 0.9, 1.0, 1.0, 1.0, 1.0, 0.9, 0.8, 0.8, 1.0, 1.2, 1.25, 1.25, 1.2, 1.0, 0.8, 0.8, 1.0, 1.25, 1.3, 1.3, 1.25, 1.0, 0.8, 0.8, 1.0, 1.25, 1.3, 1.3, 1.25, 1.0, 0.8, 0.8, 1.0, 1.2, 1.25, 1.25, 1.2, 1.0, 0.8, 0.8, 0.9, 1.0, 1.0, 1.0, 1.0, 0.9, 0.8, 0.7, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.7]
+KINGSPACE = 8
+
 
 
 whitepawn =   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -29,20 +43,32 @@ blackking =   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 blacktables = [blackpawn, blackknight, blackbishop, blackrook, blackqueen, blackking]
 
 
+
 for i in range(64):
     values_at_i = [table[i] for table in whitetables]
     if values_at_i.count(1) > 1:
         print(f"White: Shared '1' found at index {i} in {values_at_i}")
-totalwhitetable = [
-    int(any(table[i] == 1 for table in whitetables))
-    for i in range(len(whitetables[0]))
-]
 
 for i in range(64):
     values_at_i = [table[i] for table in blacktables]
     if values_at_i.count(1) > 1:
-        print(f"Black: Shared '1' found at index {i} in {values_at_i}")
+            print(f"Black: Shared '1' found at index {i} in {values_at_i}")
+
+
+
+totalwhitetable = [
+    int(any(table[i] == 1 for table in whitetables))
+    for i in range(len(whitetables[0]))]
+
 totalblacktable = [
     int(any(table[i] == 1 for table in blacktables))
-    for i in range(len(blacktables[0]))
-]
+    for i in range(len(blacktables[0]))]
+
+def white_captures():
+    shared_positions = [
+        i for i in range(64) if totalwhitetable[i] == 1 and totalblacktable[i] == 1]
+    if shared_positions:
+        if whiteturn:
+            return True
+        else:
+            return False
