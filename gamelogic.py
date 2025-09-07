157b3893d5ce26e2ae344fce72d4d546c1919d09
edwardmorley <edwardmorley2010@icloud.com>
2025-09-07 01:40:29 +0100

    Last indescribable update

--- gamelogic.py
+++ gamelogic.py
@@ -11,6 +11,7 @@ positional = 0.0
 safe = 0.0
 aware = 0.0
 controlling = 0.0
+decisive = 0.0
 
 PAWNVALUE = 1.0
 KNIGHTVALUE = 6.2
@@ -19,10 +20,10 @@ ROOKVALUE = 10.13
 QUEENVALUE = 20.14
 KINGVALUE = 200.0
 
+
 PAWNSPACE = 8
 PIECESPACE = [0.7, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.7, 0.8, 0.9, 1.0, 1.0, 1.0, 1.0, 0.9, 0.8, 0.8, 1.0, 1.2, 1.25, 1.25, 1.2, 1.0, 0.8, 0.8, 1.0, 1.25, 1.3, 1.3, 1.25, 1.0, 0.8, 0.8, 1.0, 1.25, 1.3, 1.3, 1.25, 1.0, 0.8, 0.8, 1.0, 1.2, 1.25, 1.25, 1.2, 1.0, 0.8, 0.8, 0.9, 1.0, 1.0, 1.0, 1.0, 0.9, 0.8, 0.7, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.7]
 KINGSPACE = 8
-
 
 
 whitepawn =   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -32,7 +33,6 @@ whiterook =   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 whitequeen =  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 whiteking =   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 whitetables = [whitepawn, whiteknight, whitebishop, whiterook, whitequeen, whiteking]
-
 
 blackpawn =   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 blackknight = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -55,7 +55,6 @@ for i in range(64):
             print(f"Black: Shared '1' found at index {i} in {values_at_i}")
 
 
-
 totalwhitetable = [
     int(any(table[i] == 1 for table in whitetables))
     for i in range(len(whitetables[0]))]
@@ -65,9 +64,9 @@ totalblacktable = [
     for i in range(len(blacktables[0]))]
 
 def white_captures():
-    shared_positions = [
+    sharedpositions = [
         i for i in range(64) if totalwhitetable[i] == 1 and totalblacktable[i] == 1]
-    if shared_positions:
+    if sharedpositions:
         if whiteturn:
             return True
         else:
